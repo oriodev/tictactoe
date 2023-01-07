829854aab541089be5f693965b86cd71c2e0f274
ori <93719767+oriodev@users.noreply.github.com>
2023-01-07 12:29:17 +0000

    added a button, gave it an event listener, and added a title with animations

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const gameBoard = ( () => {
 
-    let board = ["X", "O", "", "", "", "", "", "", ""];
+    let board = ["", "", "", "", "", "", "", "", ""];
     let allBoxes = document.querySelectorAll('[id=box]');
 
     const populateBoxes = () => {
@@ -25,9 +25,24 @@ const gameBoard = ( () => {
 
 })();
 
+// PLAYER OBJECTS
+
+const players = (playerSymbol) => {
+    const sayHello = () => {
+        console.log(`hi bitches, i'm ${playerSymbol}`);
+    }
+
+    return {
+        sayHello
+    };
+}
+
 // GAME LOOP
 
 const game = ( () => {
+
+    const crosses = players("X");
+    const noughts = players("O");
 
     const gameWinner = () => {
         // checks if someone has won
@@ -56,29 +71,67 @@ const game = ( () => {
         }
     }
 
+    // this adds an event listener to the boxes
+    const changeDom = (playerTurn) => {
+
+        let allBoxes = gameBoard.allBoxes;
+
+        for (let i = 0; i < allBoxes.length; i++) {
+            allBoxes[i].addEventListener("click", function(event) {
+                
+                if (gameBoard.board[i] == "") {
+                    gameBoard.board[i] = playerTurn.symbol;
+                    gameBoard.populateBoxes();
+                }
+            }
+
+        )}
+    }
+
+    const makeMove = (playerTurn) => {
+        changeDom(playerTurn);
+        
+    }
+
 
     const gameLoop = () => {
 
-        console.log(gameBoard.board);
-        console.log("we're in gameLoop");
-        console.log(gameEnd());
+        // reset the board
+        gameBoard.board = ["", "", "", "", "", "", "", "", ""];
 
         while (gameEnd() == false) {
             console.log("game is running");
             
             // enter gameloop logic here
 
+            let turn = crosses;
+
+            // trying to make turns work idk        
+
+
+            // failsafe don't remove
+            gameBoard.board = ["X", "O", "X", "X", "X", "O", "O", "O", "X"];
 
         }
 
-        console.log("we're on the other side of the actual loop");
+        console.log("we're on the other side of the actual loop"); 
 
     }
 
+    const startGame = () => {
+        gameLoop();
+    }
+
     return {
-        gameLoop
+        startGame,
+        crosses,
+        noughts
     }
 
 })();
 
-game.gameLoop();
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(event) {
+    game.startGame();
+});
